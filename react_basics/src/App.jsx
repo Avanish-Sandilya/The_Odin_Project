6b29b1aee1f4ab20a44c454f376db60907a1f9c4
avanish_sandilya <bhanusandilya74@gmail.com>
2026-05-20 14:15:36 +0530

    adding components

--- react_basics/src/App.jsx
+++ react_basics/src/App.jsx
@@ -1,7 +1,26 @@
+const Hello=(props)=>{
+const now = new Date()
+return(
+  <p>Hello {props.name} its {now.toString()}</p>
+)
+}
+
+const Add = (props) =>{
+return(
+  <p>{props.a} plus {props.b} is {props.a + props.b}</p>
+)
+}
+
+
+
 const App = () => {
+const a = 10
+const b = 20
+
   return (
     <div>
-      <p>Hello world</p>
+      <Hello name="Steve"/>
+      <Add a={a} b={b}/>
     </div>
   )
 }
